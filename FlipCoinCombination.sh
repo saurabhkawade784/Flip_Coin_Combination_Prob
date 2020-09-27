72b0752cb1adb36e6c818db768f9c7f13f25e269
saurabhkawade784 <saurabhkawade784@gmail.com>
2020-09-27 12:28:02 +0530

    Calculate percentage of singlet combination

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -5,10 +5,26 @@ echo "Welcome to flip coin combination problem."
 #Variables
 Head=1;
 Tail=0;
-Flip_Coin=$((RANDOM%2))
-if [[ $Flip_Coin -eq $Heads ]]
-then
-	echo "It's head"
-else
-	echo "It's tails"
-fi
+Count_Of_Heads=0;
+Count_Of_Tails=0;
+#diactionary declaration
+declare -A Singlet
+for (( i=0; i<=5; i++ ))
+do
+
+	Flip_Coin=$((RANDOM%2))
+	case $Flip_Coin in
+		$Head) Singlet["Heads"]=$((++Count_Of_Heads)) 
+		    ;;
+		$Tail) Singlet["Tails"]=$((++Count_Of_Tails)) 
+		    ;;
+		    *) echo  " "
+		    ;;
+	esac
+	((No_Of_Times--))
+done
+#read dictinaery elements.
+for pos in ${!Singlet[@]}
+do
+	echo "$pos is: ${Singlet[$pos]},"
+done
