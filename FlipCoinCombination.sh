b5d91e1283d0914f4cbfb3bbdaffcc40fe42bf37
saurabhkawade784 <saurabhkawade784@gmail.com>
2020-09-27 12:23:05 +0530

    displaying head or tail

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -1,3 +1,14 @@
-#/bin/bash
-#Displying welcome to FlipCoin Comutation program
-echo "Welcome To Flip coin computation program"
+#!/bin/bash
+#This program display winner head anf tail and their combination problem.
+#displaying wecome message
+echo "Welcome to flip coin combination problem."
+#Variables
+Head=1;
+Tail=0;
+Flip_Coin=$((RANDOM%2))
+if [[ $Flip_Coin -eq $Heads ]]
+then
+	echo "It's head"
+else
+	echo "It's tails"
+fi
